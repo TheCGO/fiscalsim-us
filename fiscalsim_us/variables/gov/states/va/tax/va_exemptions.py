66703248db1189514f828feba5a33c673cc7423d
Austin Francis <austin.francis@usu.edu>
2022-09-28 18:04:46 -0600

    started the va exemptions.py variable file

--- fiscalsim_us/variables/gov/states/va/tax/va_exemptions.py
+++ fiscalsim_us/variables/gov/states/va/tax/va_exemptions.py
@@ -1,17 +1,42 @@
-from openfisca_us.model_api import *
+from fiscalsim_us.model_api import *
 
 
-class ut_taxpayer_credit(Variable):
+class va_exemptions(Variable):
     value_type = float
     entity = TaxUnit
-    label = "UT taxpayer credit"
+    label = "VA exemptions"
     unit = USD
     definition_period = YEAR
-    defined_for = StateCode.UT
+    defined_for = StateCode.VA
 
     def formula(tax_unit, period, parameters):
         ut_taxpayer_credit_max = tax_unit("ut_taxpayer_credit_max", period)
         ut_taxpayer_credit_reduction = tax_unit(
             "ut_taxpayer_credit_reduction", period
         )
-        return ut_taxpayer_credit_max - ut_taxpayer_credit_reduction
+
+        va_indiv_exempt_multiplier = tax_unit("va_indiv_exemption_multiplier",
+                                                period)
+
+        va_indiv_p65_exempt_multiplier = tax_unit("va_indiv_exemption_over_65_multiplier",period)
+
+        dependents = tax_unit("tax_unit_dependents",period)
+
+        spouse_if_filing_jointly = ?
+
+        you_p65 = ?
+
+        spouse_p65 = ? # check to see if this is valid if they are filing separately 
+
+        you_blind = ? 
+
+        spouse_blind = ? 
+
+
+        total_section_A = ((1 + spouse_if_filing_jointly + dependents) * 
+                            va_indiv_exempt_multiplier)
+
+        total_section_B = ((you_p65 + spouse_p65 + you_blind + spouse_blind)*
+                            va_indiv_p65_exempt_multiplier)
+
+        return total_section_A + total_section_B
