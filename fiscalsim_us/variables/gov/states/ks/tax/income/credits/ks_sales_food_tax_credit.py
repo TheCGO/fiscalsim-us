31b4098fdf9b66a486348cbe737c3399dda31516
Patrick Neyland <patrick.neyland75@gmail.com>
2022-10-25 20:50:09 -0600

    added food sales tax credit qualifiers

--- fiscalsim_us/variables/gov/states/ks/tax/income/credits/ks_sales_food_tax_credit.py
+++ fiscalsim_us/variables/gov/states/ks/tax/income/credits/ks_sales_food_tax_credit.py
@@ -10,13 +10,13 @@ class ks_sales_food_tax_credit(Variable):
     defined_for = StateCode.KS
 
     def formula(tax_unit, period, parameters):
-        p = parameters(period).gov.states.ks.tax.income.credits
+        p = parameters(period).gov.states.ks.tax.income.credits.food_sales_tax_credit
         ks_exemptions_claimed = tax_unit("ks_exemptions_claimed", period)
         dependents = tax_unit("tax_unit_dependents", period)
-        multiplier = p.ks_food_sales_tax_credit_multiplier
+        multiplier = p.multiplier
 
         line_g = ks_exemptions_claimed - dependents
 
         return line_g * multiplier
 
-#still need to add qualifiers for credit
+#still need to add qualifiers if statement
