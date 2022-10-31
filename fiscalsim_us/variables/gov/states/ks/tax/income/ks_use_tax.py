e755721369df7d1c975cd6eb712ddd794cdc4129
Patrick Neyland <patrick.neyland75@gmail.com>
2022-10-31 13:51:18 -0600

    added ks use tax

--- fiscalsim_us/variables/gov/states/ks/tax/income/ks_use_tax.py
+++ fiscalsim_us/variables/gov/states/ks/tax/income/ks_use_tax.py
@@ -1,7 +1,7 @@
 from fiscalsim_us.model_api import *
 
 
-class ca_use_tax(Variable):
+class ks_use_tax(Variable):
     value_type = float
     entity = TaxUnit
     label = "CA Use Tax"
@@ -11,10 +11,19 @@ class ca_use_tax(Variable):
     defined_for = StateCode.CA
 
     def formula(tax_unit, period, parameters):
-        income = tax_unit("ca_agi", period)
-        p = parameters(period).gov.states.ca.tax.income.use_tax
-        # Compute main amount, a dollar amount based on CA AGI.
+        income = tax_unit("ks_agi", period)
+        p = parameters(period).gov.states.ks.tax.income.use_tax
+        # Compute main amount, a dollar amount based on KS AGI.
         main_amount = p.main.calc(income)
         # Switches to a percentage of income above the top main threshold.
-        additional_amount = p.additional.calc(income) * income
-        return main_amount + additional_amount
+        additional = income * p.multiplier
+        
+        if income > p.threshold:
+
+            return main_amount
+
+        else:
+
+            return additional
+        
+
