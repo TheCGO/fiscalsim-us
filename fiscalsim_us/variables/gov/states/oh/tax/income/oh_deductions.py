c79c71164a47f61308954dece68bb2e276fa1a5d
Patrick Neyland <patrick.neyland75@gmail.com>
2022-11-07 20:54:47 -0700

    added notebook to start testing simulations and adjusted variabels

--- fiscalsim_us/variables/gov/states/oh/tax/income/oh_deductions.py
+++ fiscalsim_us/variables/gov/states/oh/tax/income/oh_deductions.py
@@ -7,5 +7,5 @@ class oh_deductions(Variable):
     label = "OH deductions"
     unit = USD
     definition_period = YEAR
-    defined_for = StateCode.KS
+    defined_for = StateCode.OH
     reference = "https://tax.ohio.gov/static/forms/ohio_individual/individual/2021/pit-it1040-booklet.pdf" #page 14
