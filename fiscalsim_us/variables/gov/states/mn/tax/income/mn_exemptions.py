894bc52cc4aeb7f87fed10b1bf1c4b697f892245
mitchellpound <mitchell.pound@usu.edu>
2023-02-25 12:07:47 -0700

    Added vars and pararms for dependent exemption

--- fiscalsim_us/variables/gov/states/mn/tax/income/mn_exemptions.py
+++ fiscalsim_us/variables/gov/states/mn/tax/income/mn_exemptions.py
@@ -1,4 +1,5 @@
 from fiscalsim_us.model_api import *
+from math import ceil
 
 
 class mn_exemptions(Variable):
@@ -13,5 +14,22 @@ class mn_exemptions(Variable):
     defined_for = StateCode.MN
 
     def formula(tax_unit, period, parameters):
+        p = (
+            period
+        ).gov.states.mn.tax.income.exemptions
 
-        return 0
+        fed_agi = tax_unit("adjusted_gross_income", period)
+        dependents = tax_unit("tax_unit_dependents", period)
+        filing_status = tax_unit("filing_status", period)
+        amount_per_depend = p.dependent
+        threshold = p.dependent_phaseout[filing_status]
+
+        if fed_agi <= threshold:
+            return dependents * amount_per_depend
+        
+        extra = fed_agi - threshold
+        divisor = p.dependent_phase_divisor[filing_status] 
+
+        return max(dependents * amount_per_depend * (1 - ceil(extra / divisor) * .02),
+            0
+        )
