2a872d2a3d652f12220fa01f760c50e72cba69ca
mitchellpound <mitchell.pound@usu.edu>
2023-03-01 00:51:58 -0700

    Added logic/ documentation to credits

--- fiscalsim_us/variables/gov/states/mn/tax/income/credits/mn_nonrefundable_credits.py
+++ fiscalsim_us/variables/gov/states/mn/tax/income/credits/mn_nonrefundable_credits.py
@@ -3,7 +3,26 @@ from fiscalsim_us.model_api import *
 
 class mn_nonrefundable_credits(Variable):
     """
-    TODO: SOMETHING HERE
+    Line 16 on Minnesota 2022 Indivual Tax Return from M1. 
+    These cretis include the following categories and are listed on 
+    form M1C at https://www.revenue.state.mn.us/sites/default/files/2023-01/m1c_22.pdf
+    * Marriage credit
+    * Credit for long term care insurcance premiums paid
+    * Credit for taxes paid to another state
+    * Credit for past military service
+    * Employer Transit Pass Credit
+    * SEED Capital Investment Credit
+    * Education Savings Account Contribution Credit
+    * Credit for attaining masters degree in teacher licensure field
+    * Student Loan Credit
+    * Beginning Farmer Management Credit
+    * Film Production Credit
+    * Tax Credit for Owners of Agricultural Assets
+    * Credit for increasing research activities
+    * Carryforward of previous year Farmer Management Credits
+    * Carryforward of previous year Owners of Agricultural Assets Credits 
+    * Carryforward of prior year increasing research activities credit
+    * Alternative Minimum Tax Credit
     """
     value_type = float
     entity = TaxUnit
@@ -12,6 +31,3 @@ class mn_nonrefundable_credits(Variable):
     definition_period = YEAR
     defined_for = StateCode.MN
 
-    def formula(tax_unit, period, parameters):
-
-        return 0
