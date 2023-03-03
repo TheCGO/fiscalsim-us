02a39a933fbe44677513d4b1e417c42328d6bcfc
Richard Evans <rickecon@gmail.com>
2023-03-02 23:14:34 -0700

    Update state_income_tax_before_refundable_credits.py
    Added `ut_income_tax_before_refundable_credits` back into `variables/gov/states/tax/income/state_income_tax_before_refundable_credits.py`.

--- fiscalsim_us/variables/gov/states/tax/income/state_income_tax_before_refundable_credits.py
+++ fiscalsim_us/variables/gov/states/tax/income/state_income_tax_before_refundable_credits.py
@@ -15,5 +15,6 @@ class state_income_tax_before_refundable_credits(Variable):
         "ny_income_tax_before_refundable_credits",
         "or_income_tax_before_refundable_credits",
         "pa_income_tax",  # PA has no refundable credits.
+        "ut_income_tax_before_refundable_credits",
         "wa_income_tax_before_refundable_credits",
     ]
