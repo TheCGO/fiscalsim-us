163bb22e03a5a5280867f19a420ebd1cd712ca14
Austin Francis <austin.francis@usu.edu>
2023-03-22 10:56:54 -0600

    renamed some variables and added them to the correct files so it connects it to the rest of the model, I also changed the test_microsim.py file

--- fiscalsim_us/variables/gov/states/tax/income/state_income_tax.py
+++ fiscalsim_us/variables/gov/states/tax/income/state_income_tax.py
@@ -13,8 +13,9 @@ class state_income_tax(Variable):
         "ny_income_tax",
         "or_income_tax",
         "pa_income_tax",
-        "wa_income_tax",
+        "va_income_tax",
         "ut_income_tax",
+        "wa_income_tax",
     ]
 
     def formula(tax_unit, period, parameters):
