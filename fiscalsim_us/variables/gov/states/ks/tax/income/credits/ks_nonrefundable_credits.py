b6ebb57296ede5cfb2980ecb6a34cd356448408f
Patrick Neyland <patrick.neyland75@gmail.com>
2023-03-28 09:23:32 -0600

    changed formatting for how credits are referenced

--- fiscalsim_us/variables/gov/states/ks/tax/income/credits/ks_nonrefundable_credits.py
+++ fiscalsim_us/variables/gov/states/ks/tax/income/credits/ks_nonrefundable_credits.py
@@ -2,23 +2,27 @@ from fiscalsim_us.model_api import *
 
 
 class ks_nonrefundable_credits(Variable):
-    """
-    Sum of lines 13 through 18 on Kansas 2022 Individual Income Tax return form K-40.
-    https://www.ksrevenue.gov/pdf/k-4022.pdf
-
-    """
-
     value_type = float
     entity = TaxUnit
     label = "KS nonrefundable credits"
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.KS
+    adds = "gov.states.ks.tax.income.credits.non_refundable"
 
-adds = ["ks_eitc"]
+    # def formula(tax_unit, period, parameters):
+    #     ks_eitc = tax_unit("ks_eitc", period)
+    #     child_credit = tax_unit("ks_child_expense_credit", period)
+    #     return ks_eitc + child_credit
+
 
 # TODO: Add credits for:
 # * Line 17: Kansas earned income credit
 # * Line 14: Kansas child and dependent care credit
 # * Line 18: Food Sales Tax Credit
 # * Lines 13 & 15: Other credits
+    # """
+    # Sum of lines 13 through 18 on Kansas 2022 Individual Income Tax return form K-40.
+    # https://www.ksrevenue.gov/pdf/k-4022.pdf
+
+    # """
--- fiscalsim_us/variables/gov/states/ks/tax/income/ks_income_tax_before_refundable_credits.py
+++ fiscalsim_us/variables/gov/states/ks/tax/income/ks_income_tax_before_refundable_credits.py
@@ -18,4 +18,4 @@ class ks_income_tax_before_refundable_credits(Variable):
         )
         credits = tax_unit("ks_nonrefundable_credits", period)
 
-        return max_(income_tax_before_credits - credits, 0)
+        return max_(0, income_tax_before_credits - credits)
