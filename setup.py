ce1f8c1ac17fc958da9c1a59d8ef1bfd4289d8b7
Richard Evans <rickecon@gmail.com>
2023-06-21 01:05:52 -0600

    Updated pr.yaml and setup.py

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
         ],
     },
     # Windows CI requires Python 3.9.
-    python_requires=">=3.7",
+    python_requires=">=3.7, <3.11",
     entry_points={
         "console_scripts": [
             "fiscalsim-us = fiscalsim_us.tools.cli:main",
