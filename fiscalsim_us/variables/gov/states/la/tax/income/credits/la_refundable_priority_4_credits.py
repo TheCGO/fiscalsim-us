183c61263968de5640caf3d4a4c508018be9d083
mitchellpound <mitchell.pound@usu.edu>
2023-07-03 12:01:53 -0600

    Added LA refundable priority 4 doc string

--- fiscalsim_us/variables/gov/states/la/tax/income/credits/la_refundable_priority_4_credits.py
+++ fiscalsim_us/variables/gov/states/la/tax/income/credits/la_refundable_priority_4_credits.py
@@ -2,6 +2,13 @@ from fiscalsim_us.model_api import *
 
 
 class la_refundable_priority_4_credits(Variable):
+    """
+    louisiana refundable priority 4 credits 
+    Line 25 on Form IT-540. These credits include the 
+    following with codes:
+    * 50F - Inventory Tax
+    * 51F - Ad Valorem Natural Gas
+    """
     value_type = float
     entity = TaxUnit
     label = "Louisiana refundable priority 4 income tax credits"
