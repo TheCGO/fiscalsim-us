b66173bef195b0ef334f91910e34cf85459cd907
mitchellpound <mitchell.pound@usu.edu>
2023-08-07 19:52:45 -0600

    Created tests for LA credits

--- fiscalsim_us/variables/gov/states/la/tax/income/credits/la_refundable_childcare.py
+++ fiscalsim_us/variables/gov/states/la/tax/income/credits/la_refundable_childcare.py
@@ -17,11 +17,24 @@ class la_refundable_childcare(Variable):
 
         p = parameters(period).gov.states.la.tax.income.credits.refundable_p2
         agi = tax_unit("adjusted_gross_income", period)
-        earned_income = tax_unit("tax_unit_earned_income", period)
+        person = tax_unit.members
+        dependent = person("is_tax_unit_dependent", period)
+        earned_income = max_(0, person("earned", period))
+        filing_status = tax_unit("filing_status", period)
+        non_dependent_income = ~dependent * earned_income
+
+        # if tax unit is joint then pick the minimum of the non-dependent
+        # earned incomes, else pick the (only) max non-dependent income
+        tax_unit_min_income = where(
+            filing_status == filing_status.possible_values.JOINT,
+            tax_unit.sum(non_dependent_income) - tax_unit.max(non_dependent_income),
+            tax_unit.max(non_dependent_income)
+            )
+
+        # earned_income = tax_unit("tax_unit_earned_income", period)
         childcare_expenses = tax_unit("tax_unit_childcare_expenses", period)
 
         # check for eligible children
-        person = tax_unit.members
         dependent = person("is_tax_unit_dependent", period)
         age = person("age", period)
         child = age < p.childcare_child_age_limit
@@ -32,7 +45,7 @@ class la_refundable_childcare(Variable):
         expense = min_(childcare_expenses, max_expense)
 
         # line 6 of worksheet
-        credit = min_(expense, earned_income)
+        credit = min_(expense, tax_unit_min_income)
 
         income_mult = p.childcare_income_mult.calc(agi)
         mult = p.refundable_childcare_mult
