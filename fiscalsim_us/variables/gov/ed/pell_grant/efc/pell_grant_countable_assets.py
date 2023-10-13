0b619845cf80d3c725e87e0fc012b06ef2efe1de
Richard Evans <rickecon@gmail.com>
2023-10-13 03:06:17 -0600

    Main commit of all updated files

---
+++ fiscalsim_us/variables/gov/ed/pell_grant/efc/pell_grant_countable_assets.py
@@ -0,0 +1,9 @@
+from fiscalsim_us.model_api import *
+
+
+class pell_grant_countable_assets(Variable):
+    value_type = float
+    entity = Person
+    label = "Pell Grant countable assets"
+    unit = USD
+    definition_period = YEAR
