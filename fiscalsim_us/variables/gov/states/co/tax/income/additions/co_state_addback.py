60339a60c2745c6e9964939dc59e58f6671bd802
mitchellpound <mitchell.pound@usu.edu>
2023-10-21 09:23:48 -0600

    Fixed circularity for CO in state_income_tax var

--- fiscalsim_us/variables/gov/states/co/tax/income/additions/co_state_addback.py
+++ fiscalsim_us/variables/gov/states/co/tax/income/additions/co_state_addback.py
@@ -15,18 +15,8 @@ class co_state_addback(Variable):
 
     def formula(tax_unit, period, parameters):
         federal_itemizer = tax_unit("tax_unit_itemizes", period)
-        state_inctax = max_(0, tax_unit("state_income_tax", period))
-        property_taxes = add(tax_unit, period, ["real_estate_taxes"])
-        # follow worksheet on page 5 of 2021 Book cited above:
-        irs_schA_line_5d = state_inctax + property_taxes
-        irs_schA_line_5e = tax_unit("salt_deduction", period)
-        ws_line_a = where(
-            irs_schA_line_5d > irs_schA_line_5e,
-            max_(0, irs_schA_line_5e - property_taxes),
-            state_inctax,
-        )
-        p = parameters(period).gov.irs.deductions
-        ws_line_b = add(tax_unit, period, p.itemized_deductions)
-        ws_line_c = tax_unit("standard_deduction", period)
-        ws_line_d = max_(0, ws_line_b - ws_line_c)
-        return federal_itemizer * min_(ws_line_a, ws_line_d)
+        # Colorado only requires taxpayers to add back state income tax from
+        # Federal Schedule A (line 5a). It does not require taxpayers to
+        # add back state real estate or property tax from Schedule A
+        state_inctax = max_(0, tax_unit("statelocal_sales_or_prior_inctax", period))
+        return federal_itemizer * state_inctax
--- fiscalsim_us/variables/gov/states/tax/income/state_income_tax.py
+++ fiscalsim_us/variables/gov/states/tax/income/state_income_tax.py
@@ -10,7 +10,7 @@ class state_income_tax(Variable):
     adds = [
         # state income tax variables listed in alphabetical order:
         "ca_income_tax",
-        # "co_income_tax",  --- activating will cause circular logic errors
+        "co_income_tax",  #--- activating will cause circular logic errors
         "dc_income_tax",
         "ia_income_tax",
         "il_income_tax",
