1576d9eec18a3c6fd31572bbe8db0a86994b30f0
Richard Evans <rickecon@gmail.com>
2023-10-24 23:49:22 -0600

    Updated main folder variables with file name not matching variable name

--- fiscalsim_us/variables/gov/states/va/tax/income/va_disability_income_reported_as_wages.py
+++ fiscalsim_us/variables/gov/states/va/tax/income/va_disability_income_reported_as_wages.py
@@ -1,7 +1,7 @@
 from fiscalsim_us.model_api import *
 
 
-class disability_income_reported_as_wages(Variable):
+class va_disability_income_reported_as_wages(Variable):
     value_type = float
     entity = TaxUnit
     label = "VA disability income reported as wages - https://www.tax.virginia.gov/sites/default/files/taxforms/individual-income-tax/2021/760-2021.pdf"
