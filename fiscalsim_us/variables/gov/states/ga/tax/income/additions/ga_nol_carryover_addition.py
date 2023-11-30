6c27126d1933e499bbbfedc381fa8e3c1b8d5515
mitchellpound <mitchell.pound@usu.edu>
2023-11-30 11:19:18 -0700

    Added some GA additions to income

---
+++ fiscalsim_us/variables/gov/states/ga/tax/income/additions/ga_nol_carryover_addition.py
@@ -0,0 +1,13 @@
+from fiscalsim_us.model_api import *
+
+
+class ga_nol_carryover_addition(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "Georgia addition to incone for net operating loss carryover deducted on federal return"
+    unit = USD
+    definition_period = YEAR
+    reference = (
+        "https://houpl.org/wp-content/uploads/2023/01/2022-IT-511_Individual_Income_Tax_-Booklet-compressed.pdf#page=34"
+    )
+    defined_for = StateCode.GA
---
+++ fiscalsim_us/variables/gov/states/ga/tax/income/additions/ga_other_additions.py
@@ -0,0 +1,17 @@
+from fiscalsim_us.model_api import *
+
+
+class ga_other_additions(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "Georgia other additions to incone"
+    unit = USD
+    definition_period = YEAR
+    reference = (
+        "https://houpl.org/wp-content/uploads/2023/01/2022-IT-511_Individual_Income_Tax_-Booklet-compressed.pdf#page=34"
+    )
+    defined_for = StateCode.GA
+
+    # For line 5 of Georgia tax form 500 Schedule 1
+    # Also a catch-all for any non-implemented additions
+    # Such as interest on non-Georgia municipal or state bonds
