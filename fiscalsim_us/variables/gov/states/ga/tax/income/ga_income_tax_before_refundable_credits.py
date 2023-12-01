ae48b51591378adc992441f9b619659470877bd6
mitchellpound <mitchell.pound@usu.edu>
2023-12-01 11:19:51 -0700

    Fixed non-refundable credits

--- fiscalsim_us/variables/gov/states/ga/tax/income/ga_income_tax_before_refundable_credits.py
+++ fiscalsim_us/variables/gov/states/ga/tax/income/ga_income_tax_before_refundable_credits.py
@@ -10,3 +10,9 @@ class ga_income_tax_before_refundable_credits(Variable):
     defined_for = StateCode.GA
     adds = ["ga_income_tax_before_credits"]
     subtracts = ["ga_nonrefundable_credits"]
+
+    def formula(tax_unit, period, parameters):
+        tax_before_credits = tax_unit("ga_income_tax_before_credits", period)
+        nonrefundables = tax_unit("ga_nonrefundable_credits", period)
+
+        return max_(0, tax_before_credits - nonrefundables)
