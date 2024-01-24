5a4117f719d0e9c08e6b2bbc93b118360e05f63c
Brenden Bodily <brendenbodily@gmail.com>
2024-01-24 16:28:52 -0700

    test without ira exemption

--- fiscalsim_us/variables/gov/states/ar/tax/income/ar_total_income.py
+++ fiscalsim_us/variables/gov/states/ar/tax/income/ar_total_income.py
@@ -20,7 +20,7 @@ class ar_total_income(Variable):
         not_dependent = ~person("is_tax_unit_dependent", period)
         for source in sources:
             # Add positive values only - losses are deducted later.
-           total += where(source in retirement_sources,  not_dependent * max_(0, add(person, period, [source])-ira_exemption),not_dependent * max_(0, add(person, period, [source])))
+            total += not_dependent * max_(0, add(person, period, [source]))
+           # total += where(source in retirement_sources,  not_dependent * max_(0, add(person, period, [source])-ira_exemption),not_dependent * max_(0, add(person, period, [source])))
             
         return total
-    
