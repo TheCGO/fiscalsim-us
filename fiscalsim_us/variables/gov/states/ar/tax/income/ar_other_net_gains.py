ca8f292de40b18572cadd4bffbf75f64ecdda38f
Brenden Bodily <brendenbodily@gmail.com>
2024-02-15 14:01:09 -0700

    fixed source title

--- fiscalsim_us/variables/gov/states/ar/tax/income/ar_other_net_gains.py
+++ fiscalsim_us/variables/gov/states/ar/tax/income/ar_other_net_gains.py
@@ -1,7 +1,7 @@
 from fiscalsim_us.model_api import *
 
 
-class ar_other_net_gain(Variable):
+class ar_other_net_gains(Variable):
     value_type = float
     entity = TaxUnit
     label = "Other net gains"
