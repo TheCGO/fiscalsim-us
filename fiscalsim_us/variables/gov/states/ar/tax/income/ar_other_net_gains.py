53174c0097a8e94bdda31adb381f7ef1f9585467
Brenden Bodily <brendenbodily@gmail.com>
2024-02-15 14:06:42 -0700

    fixed entity status

--- fiscalsim_us/variables/gov/states/ar/tax/income/ar_other_net_gains.py
+++ fiscalsim_us/variables/gov/states/ar/tax/income/ar_other_net_gains.py
@@ -3,7 +3,7 @@ from fiscalsim_us.model_api import *
 
 class ar_other_net_gains(Variable):
     value_type = float
-    entity = TaxUnit
+    entity = Person
     label = "Other net gains"
     unit = USD
     documentation = "Other net gain/loss from Federal Form 4797"
