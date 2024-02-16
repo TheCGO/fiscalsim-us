034506c4b2a3007e4d5806b2cc7741e89d46b521
Brenden Bodily <brendenbodily@gmail.com>
2024-02-16 14:57:46 -0700

    added capital loss adjustment to total income py file

--- fiscalsim_us/variables/gov/states/ar/tax/income/ar_agi.py
+++ fiscalsim_us/variables/gov/states/ar/tax/income/ar_agi.py
@@ -11,5 +11,5 @@ class ar_agi(Variable):
     # Arkansas Code
     reference = "https://law.justia.com/codes/arkansas/2019/title-26/subtitle-5/chapter-51/subchapter-4/section-26-51-403/"
 
-    adds = ["ar_total_income"]
+    adds = ["ar_total_income", "ar_capital_loss_adjustment"]
     subtracts = ["ar_income_adjustments"]
