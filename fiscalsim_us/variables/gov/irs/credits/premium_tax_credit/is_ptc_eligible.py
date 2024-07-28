e92e74f8109d1a50115ed788f7ce2351c0eb294c
Colton Leese <coltleese@gmail.com>
2024-07-28 11:40:45 -0700

    fixing is_ptc_eligible

--- fiscalsim_us/variables/gov/irs/credits/premium_tax_credit/is_ptc_eligible.py
+++ fiscalsim_us/variables/gov/irs/credits/premium_tax_credit/is_ptc_eligible.py
@@ -1,5 +1,5 @@
+import numpy as np
 from fiscalsim_us.model_api import *
-
 
 class is_ptc_eligible(Variable):
     value_type = bool
@@ -15,4 +15,9 @@ class is_ptc_eligible(Variable):
         on_marketplace = (
             add(tax_unit, period, ["has_marketplace_health_coverage"]) > 0
         )
-        return on_marketplace & eligibility.calc(income_level)
+        income_eligible = eligibility.calc(income_level)
+        
+        on_marketplace = np.array(on_marketplace, dtype=bool)
+        income_eligible = np.array(income_eligible, dtype=bool)
+        
+        return on_marketplace & income_eligible
