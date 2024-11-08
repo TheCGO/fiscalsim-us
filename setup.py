bae1cbe1db13cc22c93aa7d488aad422bbee58fb
Richard Evans <rickecon@gmail.com>
2024-11-07 22:59:35 -0700

    Removed tools/taxcalc directory

--- setup.py
+++ setup.py
@@ -37,7 +37,6 @@ setup(
         "microdf_python",
         "numpy>=1.24, <1.25",
         "pandas",
-        "matplotlib",
         "pathlib",
         "policyengine-core>=2.8,<2.9",
         "pytest",
